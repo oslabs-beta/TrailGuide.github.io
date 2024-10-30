216883bbfaad5909164ab162cee9fe3574c19bee
Samuel <samlarivs@outlook.com>
2024-10-30 17:53:31 -0400

    Feat:Adds Screenshots, Contact Links

--- launch-page/src/Screenshots.jsx
+++ launch-page/src/Screenshots.jsx
@@ -1,5 +1,11 @@
 
 import './Screenshots.scss';
+import Login from './assets/login.png';
+import Dashboard from './assets/dashboard.png';
+import Profile from './assets/profile.png';
+import RecentEvents from './assets/RecentEvents.png';
+import EventsModal from './assets/Events-Modal.png';
+import DarkMode from './assets/DarkMode.png';
 
 const Screenshots = () => {
   return (
@@ -7,8 +13,12 @@ const Screenshots = () => {
       <div className="container">
         <h2 className="section-title">Screenshots/Demo</h2>
         <div className="screenshot-gallery">
-          <img src="path-to-dashboard-image.jpg" alt="Dashboard" />
-          <img src="path-to-chart-image.jpg" alt="Chart" />
+          <img src={Login} alt="Login" />
+          <img src={Profile} alt="Profile" />
+          <img src={Dashboard} alt="DashBoard" />
+          <img src={RecentEvents} alt="Recent Events" />
+          <img src={EventsModal} alt="Events Modal" />
+          <img src={DarkMode} alt="Dark Mode" />
         </div>
       </div>
     </section>
